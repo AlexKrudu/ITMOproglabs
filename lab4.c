499ca25c92dd681164c6b2a0b8151f5b7ed9e7bc
AlexKrudu <KrudAlex@yandex.ru>
2019-09-14 11:03:22 +0300

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -4,17 +4,7 @@ int main(void) {
   int num, remnant;
   printf("Enter number(octal) \n");
   scanf("%o", &num);
-  if (num >= 64 && num <= 87){
-    printf("Number is in range \n");
-  }
-  else{
-    printf("Number is not in range \n");
-  }
+  printf("%d (1 - in range, 0 - not in range)\n", ((num- 87) >> 31) * ((64 - num) >> 31));
   remnant = num/4096;
-  if (num/4096 % 2 == 0){
-      printf("13rd bit is 0");
-  }  
-  else{
-    printf("13rd bit is 1");
-  }
+  printf("13rd bit is %d",num/4096 % 2);
 }
